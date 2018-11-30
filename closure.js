85350f431925f0bdf5043db9d2e2c0a77e3f7140
PSSTanusha <psstanusha@gmail.com>
2018-11-30 17:42:39 +0530

    Update closure.js

--- closure.js
+++ closure.js
@@ -8,5 +8,11 @@ function makeAdder(x) {
   var add5 = makeAdder(5);
   var add10 = makeAdder(10);
   
-  console.log(add5(2));  // 7
-  console.log(add10(2)); // 12
+  console.log(add5(2));  
+  console.log(add10(2));
+
+
+output:
+C:\Users\tanus\.vscode\extensions>node closure.js
+7
+12
