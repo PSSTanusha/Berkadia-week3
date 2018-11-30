68f21437217525bd06bbb42e1f4b3d8c8ebd5f39
PSSTanusha <psstanusha@gmail.com>
2018-11-30 17:43:44 +0530

    Update eventemitter.js

--- eventemitter.js
+++ eventemitter.js
@@ -4,3 +4,7 @@ em.on('FirstEvent', function (data) {
     console.log('First subscriber: ' + data);
 });
 em.emit('FirstEvent', 'This is my first Node.js event emitter example.');
+
+output:
+C:\Users\tanus\.vscode\extensions>node eventemitter.js
+First subscriber: This is my first Node.js event emitter example.
